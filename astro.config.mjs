9885f6340d114f28b3468596143a1606f1c2bd5f
ayushsaranGithuB <112019897+ayushsaranGithuB@users.noreply.github.com>
2026-07-15 17:51:13 +0530

    feat: Update site URL in configuration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://pm-course.ayushsaran3422.workers.dev",
   integrations: [mdx()],
 
   markdown: {
